714f526af73f267f12a7f6cf75a06f7311cecf17
kayvee <kay.vee1989@gmail.com>
2016-02-12 17:07:57 -0800

    finish lesson 6 video, pre-exercise

--- src/brainCenter.java
+++ src/brainCenter.java
@@ -1,40 +1,56 @@
+import java.util.Random;
+
 public class brainCenter 
 {
 	
 	public static void main(String[] args) 		//main compilation unit, runs first
 	{ 
-		
 		System.out.println("MAIN IS BEGINNING");
 		
-		Stock st = new Stock(); 
+		Random r = new Random();
+		System.out.println("random number is " + r.nextDouble());
 		
-		System.out.println("stock named " + st.symbol + " is priced at $" + st.price);
-		System.out.println("the industry of this stock is " + st.industry);
+		Stock l = new Stock();
+		System.out.println("stock price is " + l.price);
+		System.out.println("IPO quantity is " + l.ipoQty);
 
-		String ind = "telecom"; 
 		
-		if(st.industry.equalsIgnoreCase(ind))
+		int limit = 50000000;
+		int randomOutput = r.nextInt();
+		if(randomOutput > limit)
 		{
-			System.out.println("the industry of this stock is telecom");
+			System.out.println("random exceeds " + limit);
 		}
 		else
 		{
-			System.out.println("the industry of this stock is not telecom");
-			System.out.println("the industry of this stock is " + st.industry);
-		}	
+			System.out.println("random is under the limit, " + limit);
+		}
 		
-		int limit = 1000;
-		if(st.ipoQty < limit)
-		{
-			System.out.println("IPO QTY EXCEEDS LIMIT OF " + limit);
-		}
-		else if(st.ipoQty <= limit)
-		{
-			System.out.println("IPO QTY FALLS WITHIN LIMIT OF " + limit);
-		}
-		else if(st.ipoQty == limit)
-		{
-			System.out.println("YOUR IPO QTY IS == 50000000");
-		}
+//		Stock st = new Stock(); 
+//		String ind = "telecom"; 
+//		
+//		if(st.industry.equalsIgnoreCase(ind))
+//		{
+//			System.out.println("the industry of this stock is telecom");
+//		}
+//		else
+//		{
+//			System.out.println("the industry of this stock is not telecom");
+//			System.out.println("the industry of this stock is " + st.industry);
+//		}	
+//		
+//		int limit = 1000;
+//		if(st.ipoQty < limit)
+//		{
+//			System.out.println("IPO QTY EXCEEDS LIMIT OF " + limit);
+//		}
+//		else if(st.ipoQty <= limit)
+//		{
+//			System.out.println("IPO QTY FALLS WITHIN LIMIT OF " + limit);
+//		}
+//		else if(st.ipoQty == limit)
+//		{
+//			System.out.println("YOUR IPO QTY IS == 50000000");
+//		}
 	}
 }
