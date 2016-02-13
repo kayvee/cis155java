ce2e47b7c39ad4427c533471f3b6243b1a7ff000
kayvee <kay.vee1989@gmail.com>
2016-02-12 17:49:37 -0800

    finish lesson 7 video

--- src/Stock.java
+++ src/Stock.java
@@ -3,6 +3,9 @@
  * @author kvarjoy
  * 
  */
+
+import java.util.Random;
+
 public class Stock 
 {
 	public String symbol; //a public variable is visible outside the class by other classes
@@ -13,17 +16,36 @@ public class Stock
 						//constructor, first thing that happens when an instance of this object is created
 	public Stock() 		// POPULATE THIS STOCK WITH SOME INFORMATION
 	{
-		System.out.println("I just called the constructor...");
 		industry = "defense"; //word, string of characters
-		
-		/*
-		 * 1. define price randomly
-		 * 2. define ipo qty randomly
-		 */
-		
-		
-		price = 55.0; //price is a double, a number with decimals
-		ipoQty = 50000000; //an integer, no decimals
 		symbol = "GEL"; //for our sim, we'll assign randomly determined symbol names
+		
+		//two different ways of initializing class variables
+		setStockPrice();
+		ipoQty = setIpoQty();
 	}
+	
+	public void printPriceAndIpoqty()
+	{
+		System.out.println("price is now " + price);
+		System.out.println();
+		System.out.println("ipo qty is now " + ipoQty);
+	}
+	
+	private void setStockPrice()
+	{
+		Random p = new Random();
+		price = p.nextDouble() * 100;
+		//System.out.println("stock price is " + price);
+	}
+
+	private int setIpoQty()
+	{
+		int returnValue = 0;
+		
+		Random i = new Random();
+		returnValue = i.nextInt(15000000); //ipo is a larger # of stocks initially sold by bank
+		//System.out.println("IPO quantity is " + returnValue);
+		
+		return returnValue;
+	}	
 }
--- src/brainCenter.java
+++ src/brainCenter.java
@@ -1,56 +1,14 @@
-import java.util.Random;
-
 public class brainCenter 
 {
 	
 	public static void main(String[] args) 		//main compilation unit, runs first
 	{ 
-		System.out.println("MAIN IS BEGINNING");
-		
-		Random r = new Random();
-		System.out.println("random number is " + r.nextDouble());
 		
 		Stock l = new Stock();
-		System.out.println("stock price is " + l.price);
-		System.out.println("IPO quantity is " + l.ipoQty);
-
+		/*
+		 * Set price and ipo qty usin random numbers to init first stock
+		 */
+		l.printPriceAndIpoqty();
 		
-		int limit = 50000000;
-		int randomOutput = r.nextInt();
-		if(randomOutput > limit)
-		{
-			System.out.println("random exceeds " + limit);
-		}
-		else
-		{
-			System.out.println("random is under the limit, " + limit);
-		}
-		
-//		Stock st = new Stock(); 
-//		String ind = "telecom"; 
-//		
-//		if(st.industry.equalsIgnoreCase(ind))
-//		{
-//			System.out.println("the industry of this stock is telecom");
-//		}
-//		else
-//		{
-//			System.out.println("the industry of this stock is not telecom");
-//			System.out.println("the industry of this stock is " + st.industry);
-//		}	
-//		
-//		int limit = 1000;
-//		if(st.ipoQty < limit)
-//		{
-//			System.out.println("IPO QTY EXCEEDS LIMIT OF " + limit);
-//		}
-//		else if(st.ipoQty <= limit)
-//		{
-//			System.out.println("IPO QTY FALLS WITHIN LIMIT OF " + limit);
-//		}
-//		else if(st.ipoQty == limit)
-//		{
-//			System.out.println("YOUR IPO QTY IS == 50000000");
-//		}
 	}
 }
